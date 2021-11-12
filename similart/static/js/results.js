1ccdfcf827f457638ac772d8950fb0656c6fe3fb
Julia <juliadmartin720@gmail.com>
2021-11-11 20:26:24 -0800

    Fix edges so there are edges now yay

--- similart/static/js/results.js
+++ similart/static/js/results.js
@@ -15,25 +15,22 @@ document.onreadystatechange = function() {
   }
 };
 
-// const graphData = JSON.parse(dataElem.dataset.results);
-// Dummy data for now
-const graphData = {
-  "nodes": [{"id": 111295}, {"id": 862}, {"id": 20686}, {"id": 72951}, {"id": 119985}, {"id": 11025}, {"id": 80548}, {"id": 111428}, {"id": 15486}, {"id": 0}, {"id": 136395}, {"id": 31285}, {"id": 136190}, {"id": 40647}, {"id": 196282}, {"id": 88372}],
-  "edges": [{"source": 0, "target": 40647, "distance": 12235}, {"source": 0, "target": 88372, "distance": 13208}, {"source": 0, "target": 111295, "distance": 13369}, {"source": 0, "target": 31285, "distance": 13571}, {"source": 40647, "target": 88372, "distance": 10551}, {"source": 40647, "target": 80548, "distance": 12235}, {"source": 40647, "target": 119985, "distance": 12385}, {"source": 40647, "target": 136190, "distance": 12662}, {"source": 88372, "target": 40647, "distance": 10551}, {"source": 88372, "target": 111428, "distance": 11061}, {"source": 88372, "target": 119985, "distance": 11295}, {"source": 88372, "target": 11025, "distance": 11306}, {"source": 111295, "target": 862, "distance": 10281}, {"source": 111295, "target": 136395, "distance": 10670}, {"source": 111295, "target": 72951, "distance": 10734}, {"source": 111295, "target": 196282, "distance": 10745}, {"source": 31285, "target": 20686, "distance": 11891}, {"source": 31285, "target": 88372, "distance": 12413}, {"source": 31285, "target": 15486, "distance": 12803}, {"source": 31285, "target": 40647, "distance": 12940}]
-};
-// console.log(graphData);
-const recImage = document.getElementById('rec-image');
-
+const graphData = JSON.parse(dataElem.dataset.results);
+// const graphData = {
+//   "nodes": [{"id": 111295}, {"id": 862}, {"id": 20686}, {"id": 72951}, {"id": 119985}, {"id": 11025}, {"id": 80548}, {"id": 111428}, {"id": 15486}, {"id": 0}, {"id": 136395}, {"id": 31285}, {"id": 136190}, {"id": 40647}, {"id": 196282}, {"id": 88372}],
+//   "edges": [{"source": 0, "target": 40647, "distance": 12235}, {"source": 0, "target": 88372, "distance": 13208}, {"source": 0, "target": 111295, "distance": 13369}, {"source": 0, "target": 31285, "distance": 13571}, {"source": 40647, "target": 88372, "distance": 10551}, {"source": 40647, "target": 80548, "distance": 12235}, {"source": 40647, "target": 119985, "distance": 12385}, {"source": 40647, "target": 136190, "distance": 12662}, {"source": 88372, "target": 40647, "distance": 10551}, {"source": 88372, "target": 111428, "distance": 11061}, {"source": 88372, "target": 119985, "distance": 11295}, {"source": 88372, "target": 11025, "distance": 11306}, {"source": 111295, "target": 862, "distance": 10281}, {"source": 111295, "target": 136395, "distance": 10670}, {"source": 111295, "target": 72951, "distance": 10734}, {"source": 111295, "target": 196282, "distance": 10745}, {"source": 31285, "target": 20686, "distance": 11891}, {"source": 31285, "target": 88372, "distance": 12413}, {"source": 31285, "target": 15486, "distance": 12803}, {"source": 31285, "target": 40647, "distance": 12940}]
+// };
+console.log(graphData);
 // D3 graph here
 const width = 1000;
-const height = 700;
+const height = 800;
 
 function genUrl(imageId) {
   return `https://www.artic.edu/iiif/2/${imageId}/full/843,/0/default.jpg`;
 }
 
 d3.json('static/data/similart_data.json').then((metadata) => {
-  graphData['nodes'] = graphData['nodes'].map(img => ({ img, ...metadata[img['id'].toString()] }));
+  graphData['nodes'] = graphData['nodes'].map(img => ({ ...img, ...metadata[img['id'].toString()] }));
   console.log(graphData['nodes']);
 
   const forceGraph = d3.forceSimulation(graphData.nodes)
@@ -41,7 +38,7 @@ d3.json('static/data/similart_data.json').then((metadata) => {
     .force("link", d3.forceLink().id(d => d.id))
     .force("center", d3.forceCenter().x(width / 2).y(height / 2));
 
-  // forceGraph.force("link").links(graphData.edges).distance(d => d.distance / 100);
+  forceGraph.force("link").links(graphData.edges).distance(d => d.distance / 100);
 
   const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
@@ -50,11 +47,6 @@ d3.json('static/data/similart_data.json').then((metadata) => {
     .append("svg")
     .attr("width", width)
     .attr("height", height);
-
-  const rectangle = svg.append("rect")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("fill", "none");
 
   // Create edges as lines
   const edges = svg.selectAll("line")
@@ -92,7 +84,7 @@ d3.json('static/data/similart_data.json').then((metadata) => {
     .on("click", d => {
       document.getElementById('rec-image').setAttribute('src', genUrl(d['image_id']));
       document.getElementById('rec-title').textContent = d['title'];
-      document.getElementById('rec-artist').textContent = `By: ${d['artist_title']}`;
+      document.getElementById('rec-artist').textContent = `By: ${d['artist_title'] || 'Unknown'}`;
       document.getElementById('rec-year').textContent = d['date_display'];
       showArtDetails();
     });
