d0a52432be452e08e6a56fd589a93709818c59fe
Julia <juliadmartin720@gmail.com>
2021-11-11 20:05:38 -0800

    Changed image key to id and some css changes

--- similart/static/js/results.js
+++ similart/static/js/results.js
@@ -18,7 +18,7 @@ document.onreadystatechange = function() {
 // const graphData = JSON.parse(dataElem.dataset.results);
 // Dummy data for now
 const graphData = {
-  "nodes": [{"image": 111295}, {"image": 862}, {"image": 20686}, {"image": 72951}, {"image": 119985}, {"image": 11025}, {"image": 80548}, {"image": 111428}, {"image": 15486}, {"image": 0}, {"image": 136395}, {"image": 31285}, {"image": 136190}, {"image": 40647}, {"image": 196282}, {"image": 88372}],
+  "nodes": [{"id": 111295}, {"id": 862}, {"id": 20686}, {"id": 72951}, {"id": 119985}, {"id": 11025}, {"id": 80548}, {"id": 111428}, {"id": 15486}, {"id": 0}, {"id": 136395}, {"id": 31285}, {"id": 136190}, {"id": 40647}, {"id": 196282}, {"id": 88372}],
   "edges": [{"source": 0, "target": 40647, "distance": 12235}, {"source": 0, "target": 88372, "distance": 13208}, {"source": 0, "target": 111295, "distance": 13369}, {"source": 0, "target": 31285, "distance": 13571}, {"source": 40647, "target": 88372, "distance": 10551}, {"source": 40647, "target": 80548, "distance": 12235}, {"source": 40647, "target": 119985, "distance": 12385}, {"source": 40647, "target": 136190, "distance": 12662}, {"source": 88372, "target": 40647, "distance": 10551}, {"source": 88372, "target": 111428, "distance": 11061}, {"source": 88372, "target": 119985, "distance": 11295}, {"source": 88372, "target": 11025, "distance": 11306}, {"source": 111295, "target": 862, "distance": 10281}, {"source": 111295, "target": 136395, "distance": 10670}, {"source": 111295, "target": 72951, "distance": 10734}, {"source": 111295, "target": 196282, "distance": 10745}, {"source": 31285, "target": 20686, "distance": 11891}, {"source": 31285, "target": 88372, "distance": 12413}, {"source": 31285, "target": 15486, "distance": 12803}, {"source": 31285, "target": 40647, "distance": 12940}]
 };
 // console.log(graphData);
@@ -29,18 +29,16 @@ const width = 1000;
 const height = 700;
 
 function genUrl(imageId) {
-  const part1 = "https://www.artic.edu/iiif/2/";
-  const part2 = "/full/843,/0/default.jpg";
-  return part1.concat(imageId, part2);
+  return `https://www.artic.edu/iiif/2/${imageId}/full/843,/0/default.jpg`;
 }
 
 d3.json('static/data/similart_data.json').then((metadata) => {
-  graphData['nodes'] = graphData['nodes'].map(img => ({ img, ...metadata[img['image'].toString()] }));
+  graphData['nodes'] = graphData['nodes'].map(img => ({ img, ...metadata[img['id'].toString()] }));
   console.log(graphData['nodes']);
 
   const forceGraph = d3.forceSimulation(graphData.nodes)
     .force("charge", d3.forceManyBody().strength(-100))
-    .force("link", d3.forceLink().id(d => d.image))
+    .force("link", d3.forceLink().id(d => d.id))
     .force("center", d3.forceCenter().x(width / 2).y(height / 2));
 
   // forceGraph.force("link").links(graphData.edges).distance(d => d.distance / 100);
@@ -69,9 +67,7 @@ d3.json('static/data/similart_data.json').then((metadata) => {
   // Preview image when hovering on node
   const nodeImage = svg.append("svg:image")
     .attr("width", 200)
-    .attr("height", 200)
-    .attr("x", 200 + 100)
-    .attr("y", height - 200);
+    .attr("height", 200);
 
   //Create nodes as circles
   const nodes = svg.selectAll("circle")
@@ -80,7 +76,7 @@ d3.json('static/data/similart_data.json').then((metadata) => {
     .append("circle")
     .attr("r", 20)
     .style("fill", d => {
-      if (d.image == 0) return "crimson";
+      if (d.id === 0) return "crimson";
     })
     .on("mouseover", function(d) {
       d3.select(this).attr("fill", "goldenrod")
@@ -123,6 +119,3 @@ overlay.addEventListener('click', () => {
   overlay.style.visibility = "hidden";
   modal.style.visibility = "hidden";
 });
-
-// Temporary code to show modal and overlay
-// showArtDetails();
